d91c84b6610a12b898ee51252b9068987815da12
sadra cheraghi <162758424+Sadra138456@users.noreply.github.com>
2026-05-03 02:45:58 +0330

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	mode := flag.String("mode", "client", "Launch mode: server or client")
 	serverAddr := flag.String("server", "185.208.172.162:443", "Server address")
-	localAddr := flag.String("local", "127.0.0.1:1080", "Local SOCKS address")
+	clientLocalAddr := flag.String("local", "0.0.0.0:1080", "Client local SOCKS5 address")
 	psk := flag.String("psk", "SUPER_SECURE_SPACE_PASS", "Pre-shared key")
 	flag.Parse()
 
