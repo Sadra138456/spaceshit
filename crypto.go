58f14dccfb6a0ccd66c6f18898411d728bbfb973
sadra cheraghi <162758424+Sadra138456@users.noreply.github.com>
2026-05-03 01:18:01 +0330

    Update crypto.go

--- crypto.go
+++ crypto.go
@@ -6,24 +6,38 @@ import (
 	"encoding/hex"
 )
 
-// buildAuthHeader constructs the authentication header:
-// [32 bytes PSK hash][2 bytes target length][N bytes target address]
-func buildAuthHeader(psk, target string) []byte {
-	pskHash := hashPSK(psk)
+func buildAuthHeader(pskHex, target string) []byte {
+	pskHash := hashPSK(pskHex, target)
 	targetBytes := []byte(target)
-	targetLen := uint16(len(targetBytes))
-
+	
 	header := make([]byte, 34+len(targetBytes))
-	copy(header[:32], pskHash)
-	binary.BigEndian.PutUint16(header[32:34], targetLen)
+	copy(header[0:32], pskHash)
+	binary.BigEndian.PutUint16(header[32:34], uint16(len(targetBytes)))
 	copy(header[34:], targetBytes)
-
+	
 	return header
 }
 
-// hashPSK converts hex PSK string to SHA256 hash
-func hashPSK(psk string) []byte {
-	pskBytes, _ := hex.DecodeString(psk)
-	hash := sha256.Sum256(pskBytes)
+func hashPSK(pskHex, target string) []byte {
+	pskBytes, _ := hex.DecodeString(pskHex)
+	combined := append(pskBytes, []byte(target)...)
+	hash := sha256.Sum256(combined)
 	return hash[:]
+}
+
+func validateAuthHeader(receivedHash []byte, pskHex, target string) bool {
+	expectedHash := hashPSK(pskHex, target)
+	return bytesEqual(receivedHash, expectedHash)
+}
+
+func bytesEqual(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
