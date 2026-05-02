72a9e2345cc3f22c6720b3398a809fb74c44e3d6
sadra cheraghi <162758424+Sadra138456@users.noreply.github.com>
2026-05-03 00:54:32 +0330

    Update crypto.go

--- crypto.go
+++ crypto.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
+)
+
+// buildAuthHeader constructs the authentication header:
+// [32 bytes PSK hash][2 bytes target length][N bytes target address]
+func buildAuthHeader(psk, target string) []byte {
+	pskHash := hashPSK(psk)
+	targetBytes := []byte(target)
+	targetLen := uint16(len(targetBytes))
+
+	header := make([]byte, 34+len(targetBytes))
+	copy(header[:32], pskHash)
+	binary.BigEndian.PutUint16(header[32:34], targetLen)
+	copy(header[34:], targetBytes)
+
+	return header
+}
+
+// hashPSK converts hex PSK string to SHA256 hash
+func hashPSK(psk string) []byte {
+	pskBytes, _ := hex.DecodeString(psk)
+	hash := sha256.Sum256(pskBytes)
+	return hash[:]
+}
