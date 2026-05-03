890be39a7044c230a4db2661355c9d22c0d3b84a
sadra cheraghi <162758424+Sadra138456@users.noreply.github.com>
2026-05-03 14:05:47 +0330

    Update crypto.go

--- crypto.go
+++ crypto.go
@@ -2,45 +2,117 @@ package main
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
 	"crypto/tls"
-	"log"
+	"encoding/hex"
+	"io"
+	"math/big"
+	"net"
+	"time"
 
 	utls "github.com/refraction-networking/utls"
 )
 
-func NewClientTLSConfig(sni string) *utls.Config {
+// GetUTLSConfig creates a uTLS config mimicking Chrome
+func GetUTLSConfig(sni string, alpn []string) *utls.Config {
 	return &utls.Config{
 		ServerName:         sni,
 		InsecureSkipVerify: true,
 		MinVersion:         tls.VersionTLS12,
+		MaxVersion:         tls.VersionTLS13,
+		NextProtos:         alpn,
 	}
 }
 
-func NewServerTLSConfig(certFile, keyFile string) (*tls.Config, error) {
-	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
-	if err != nil {
-		return nil, err
+// GeneratePadding creates random padding
+func GeneratePadding(minSize, maxSize int) []byte {
+	size := minSize
+	if maxSize > minSize {
+		diff, _ := rand.Int(rand.Reader, big.NewInt(int64(maxSize-minSize)))
+		size += int(diff.Int64())
 	}
-	return &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		MinVersion:   tls.VersionTLS12,
-	}, nil
+	padding := make([]byte, size)
+	rand.Read(padding)
+	return padding
 }
 
-func GeneratePadding(maxSize int) []byte {
-	if maxSize <= 0 {
-		return nil
+// FragmentedWrite sends data in fragments with delays
+func FragmentedWrite(conn net.Conn, data []byte, fragmentSize int, delay, jitter time.Duration) error {
+	if fragmentSize <= 0 || fragmentSize >= len(data) {
+		_, err := conn.Write(data)
+		return err
 	}
-	size := 1 + (int(randomByte()) % maxSize)
-	buf := make([]byte, size)
-	if _, err := rand.Read(buf); err != nil {
-		log.Printf("⚠️  Padding generation failed: %v", err)
+
+	for i := 0; i < len(data); i += fragmentSize {
+		end := i + fragmentSize
+		if end > len(data) {
+			end = len(data)
+		}
+
+		if _, err := conn.Write(data[i:end]); err != nil {
+			return err
+		}
+
+		if i+fragmentSize < len(data) {
+			sleepTime := delay
+			if jitter > 0 {
+				j, _ := rand.Int(rand.Reader, big.NewInt(int64(jitter)))
+				sleepTime += time.Duration(j.Int64())
+			}
+			time.Sleep(sleepTime)
+		}
 	}
-	return buf
+	return nil
 }
 
-func randomByte() byte {
-	b := make([]byte, 1)
-	rand.Read(b)
-	return b[0]
+// DeriveKey derives a key from PSK
+func DeriveKey(psk string) []byte {
+	hash := sha256.Sum256([]byte(psk))
+	return hash[:]
+}
+
+// XORCipher applies XOR encryption
+func XORCipher(data, key []byte) {
+	for i := range data {
+		data[i] ^= key[i%len(key)]
+	}
+}
+
+// SecureRead reads with timeout
+func SecureRead(conn net.Conn, buf []byte, timeout time.Duration) (int, error) {
+	conn.SetReadDeadline(time.Now().Add(timeout))
+	defer conn.SetReadDeadline(time.Time{})
+	return conn.Read(buf)
+}
+
+// SecureWrite writes with timeout
+func SecureWrite(conn net.Conn, data []byte, timeout time.Duration) error {
+	conn.SetWriteDeadline(time.Now().Add(timeout))
+	defer conn.SetWriteDeadline(time.Time{})
+	_, err := conn.Write(data)
+	return err
+}
+
+// BidirectionalCopy copies data between two connections
+func BidirectionalCopy(conn1, conn2 net.Conn) {
+	done := make(chan struct{}, 2)
+
+	copy := func(dst, src net.Conn) {
+		defer func() { done <- struct{}{} }()
+		io.Copy(dst, src)
+		dst.Close()
+		src.Close()
+	}
+
+	go copy(conn1, conn2)
+	go copy(conn2, conn1)
+
+	<-done
+	<-done
+}
+
+// GenerateXORKey generates XOR key from PSK
+func GenerateXORKey(psk string) string {
+	hash := sha256.Sum256([]byte(psk))
+	return hex.EncodeToString(hash[:])
 }
